7d446df28398a4f90385c7703f3dbf6b1cb86ac6
Will <Wi_llo@hotmail.com>
2019-12-11 16:32:38 +1100

    completed basic ui elements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import './test.css';
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
 
-console.log('test');
+// Create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+// Create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+});
+
+// Create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// Create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('.snackbar-trigger');
+
+button.addEventListener('click', (e) => {
+    snackbar.show('You clicked me! :)');
+});
